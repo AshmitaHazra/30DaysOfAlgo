35ed497eeaa05f683e5b5b21002bd4e0a1c5b769
AshmitaHazra <aashi.hazra22@gmail.com>
2021-02-15 10:38:14 +0530

    15-02-2021 day-17

---
+++ DiagonalDifference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiagonalDifference
+{
+    class DiagonalDifference
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Kangaroo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kangaroo
+{
+    class Kangaroo
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
