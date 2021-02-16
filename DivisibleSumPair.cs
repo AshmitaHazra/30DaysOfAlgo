4798996bbd91e1afe48f3c63189065dfc54679a4
AshmitaHazra <aashi.hazra22@gmail.com>
2021-02-16 10:23:07 +0530

    16-02-2021 day-18

---
+++ DivisibleSumPair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DivisibleSumPair
+{
+    class DivisibleSumPair
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ MigratingBirds.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MigratingBrids
+{
+    class MigratingBirds
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
