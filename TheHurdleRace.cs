797773f9200bb2128f366ff78d3202cb68117d40
AshmitaHazra <aashi.hazra22@gmail.com>
2021-02-19 10:02:09 +0530

    19-02-2021 Day-21

---
+++ TheHurdleRace.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TheHurdleRace
+{
+    class TheHurdleRace
+    {
+        static int hurdleRace(int k, int[] height)
+        {
+
+            int max = int.MinValue;
+            for (int i = 0; i < height.Length; i++)
+            {
+                if (max < height[i])
+                    max = height[i];
+            }
+            int result = max - k;
+            if (k > max)
+                return 0;
+            else
+                return result;
+        }
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] height = Array.ConvertAll(Console.ReadLine().Split(' '), heightTemp => Convert.ToInt32(heightTemp))
+            ;
+            int result = hurdleRace(k, height);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
---
+++ designerPDFviewer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace designerPDFviewer
+{
+    class designerPDFviewer
+    {
+        static int designerPdfViewer(int[] h, string word)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            int i = 0;
+            int val;
+            int max = int.MinValue;
+            for (char ch = 'a'; ch <= 'z'; ch++)
+            {
+                map.Add(ch, h[i]);
+                i++;
+            }
+            for (int j = 0; j < word.Length; j++)
+            {
+                word.ToCharArray();
+                char ch = word[j];
+                if (map.ContainsKey(ch))
+                {
+                    val = map[ch];
+                    if (val > max)
+                        max = val;
+                }
+            }
+            return max * word.Length;
+
+        }
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int[] h = Array.ConvertAll(Console.ReadLine().Split(' '), hTemp => Convert.ToInt32(hTemp))
+            ;
+            string word = Console.ReadLine();
+
+            int result = designerPdfViewer(h, word);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
