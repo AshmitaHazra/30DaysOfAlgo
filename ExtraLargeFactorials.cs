388a93cdaab9090dd0006272bf5449d31efecd75
AshmitaHazra <aashi.hazra22@gmail.com>
2021-02-20 10:38:43 +0530

    20-02-2021 Day-22

---
+++ ExtraLargeFactorials.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace ExtraLargeFactorials
+{
+    class ExtraLargeFactorials
+    {
+        static void extraLongFactorials(int n)
+        {
+            BigInteger fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            extraLongFactorials(n);
+        }
+    }
+}
