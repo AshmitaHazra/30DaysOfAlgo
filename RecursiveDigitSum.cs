4a4113ad79fec0091eb6e57d2bf0510b9f15c8cb
AshmitaHazra <aashi.hazra22@gmail.com>
2021-02-22 10:16:26 +0530

    22-02-2021 Day-24

---
+++ RecursiveDigitSum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecursiveDigitSum
+{
+    class RecursiveDigitSum
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
