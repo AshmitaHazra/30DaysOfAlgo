b2f3d55132323189924ed15fd2d7891541b7df76
AshmitaHazra <aashi.hazra22@gmail.com>
2021-02-25 10:17:04 +0530

    25-02-2021 Day-27

---
+++ FlatLandSpaceStation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FlatLandSpaceStation
+{
+    class FlatLandSpaceStation
+    {
+        static int flatlandSpaceStations(int n, int m, int[] c)
+        {
+            Array.Sort(c);
+            int maxDistance = c[0];
+            for (int i = 1; i < c.Length; i++)
+            {
+                int distance = (c[i] - c[i - 1]) / 2;
+                if (distance > maxDistance)
+                    maxDistance = distance;
+            }
+            int lastSpaceStationDistance = n - 1 - c[m - 1];
+            if (lastSpaceStationDistance > maxDistance)
+            {
+                maxDistance = lastSpaceStationDistance;
+            }
+            return maxDistance;
+        }
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] nm = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nm[0]);
+
+            int m = Convert.ToInt32(nm[1]);
+
+            int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+            ;
+            int result = flatlandSpaceStations(n, m, c);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
---
+++ LisasWorkbook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace Lisa_sWorkbook
+{
+    class Program
+    {
+        static int workbook(int n, int k, int[] arr)
+        {
+            int result = 0;
+            int page = 1;
+            // chapter
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int problems = arr[i];
+
+                // problems
+                for (int problem = 1; problem <= problems; problem++)
+                {
+                    if (problem == page)
+                    {
+                        result++;
+                    }
+                    if ((problem % k == 0) || problem == problems)
+                    {
+                        page++;
+                    }
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            int result = workbook(n, k, arr);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
